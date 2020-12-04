dfc41cabfea37876299e64d1cd90f8106d350a70
dam <dam@203P01>
2020-12-04 10:25:01 +0100

    Primera Version Jose Hola Mundo

--- Hola Mundo/src/hola/mundo/HolaMundo.java
+++ Hola Mundo/src/hola/mundo/HolaMundo.java
@@ -16,6 +16,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo soy José");
+        System.out.println("Editado Hola Mundo soy José");
     }
     
 }
